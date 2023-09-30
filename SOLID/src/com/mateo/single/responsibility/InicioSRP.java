329dbed71c27daccdc3a8d94291212d46db08003
JhMateo <jmgranada@unillanos.edu.co>
2023-09-29 23:06:46 -0500

    Change in name of files Main

--- SOLID/src/com/mateo/single/responsibility/InicioSRP.java
+++ SOLID/src/com/mateo/single/responsibility/InicioSRP.java
@@ -1,6 +1,6 @@
 package com.mateo.single.responsibility;
 
-public class Main {
+public class InicioSRP {
     public static void main(String[] args) {
         Libro libro = new Libro("Don Quijote de la Mancha","Miguel de Cervantes","01/01/1605");
         libro.mostrarInformacion();
